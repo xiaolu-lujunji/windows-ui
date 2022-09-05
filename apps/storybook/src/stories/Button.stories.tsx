768554ffb08189b5b82bf4001500e0b3bedcbfa8
xiaolu-lujunji <xiaolu.lujunji@gmail.com>
2022-09-05 16:17:29 +0800

    Install Storybook

---
+++ apps/storybook/src/stories/Button.stories.tsx
@@ -0,0 +1,32 @@
+import React, { Children } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from 'win-ui/button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Accent = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Accent.args = {
+  variant: 'accent',
+  disabled: false,
+  children: 'Text',
+};
+
+export const Standard = Template.bind({});
+Standard.args = {
+  variant: 'standard',
+  disabled: false,
+  children: 'Text',
+};
